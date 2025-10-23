8cf6df71005217c6c599dc4860bdecc0e0962b21
Abdoulaye Ly <abdoulayely148@gmail.com>
2025-10-23 15:21:11 +0000

    fix

--- build.sh
+++ build.sh
@@ -8,7 +8,12 @@ echo "Running migrations..."
 docker run --rm \
   -e APP_ENV=production \
   -e APP_KEY=$APP_KEY \
-  -e DATABASE_URL=$DATABASE_URL \
+  -e DB_CONNECTION=pgsql \
+  -e DB_HOST=$DB_HOST \
+  -e DB_PORT=$DB_PORT \
+  -e DB_DATABASE=$DB_DATABASE \
+  -e DB_USERNAME=$DB_USERNAME \
+  -e DB_PASSWORD=$DB_PASSWORD \
   ges-bank \
   php artisan migrate --force
 
